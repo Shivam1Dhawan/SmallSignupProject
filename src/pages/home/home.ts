55fb88598ccdce43d5254b367ef1257dd022b125
unknown <shivam.dhawan.eyt.ee>
2018-09-18 16:08:55 +0530

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,15 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class HomePage {
   det:any;
+  characterdetail:any=[]
   constructor(public navCtrl: NavController,public navParams: NavParams) {
     this.det=this.navParams.get('profile'); 
+    this.characterdetail=[
+      {name: "Jon Snow" , url:"assets/imgs/jon.png" ,likes: 15 , Comment: 3},
+      {name: "Danaerys Stormborn" , url:"assets/imgs/khaleesi.jpg" ,likes: 6 , Comment: 30},
+      
+  
+    ]
   console.log(this.det.value.name);
   console.log(this.det.value.email);
 
